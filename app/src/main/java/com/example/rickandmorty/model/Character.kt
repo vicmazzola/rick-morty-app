dabd36eda6035214f0da1d00e6984db3c2f12fa2
Victor Mazzola <victormazzola2019@gmail.com>
2025-03-23 11:56:13 -0300

    feat: add RetrofitFactory

--- app/src/main/java/com/example/rickandmorty/model/Character.kt
+++ app/src/main/java/com/example/rickandmorty/model/Character.kt
@@ -1,9 +1,11 @@
 package com.example.rickandmorty.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Character(
-    val id: Int = 0,
-    val name: String = "",
+    var id: Int = 0,
+    @SerializedName("name") var charName: String = "",
     var species: String = "",
     var image: String = "",
-    val gender: String = ""
-    )
+    var gender: String = ""
+)
---
+++ app/src/main/java/com/example/rickandmorty/service/RetrofitFactory.kt
@@ -0,0 +1,23 @@
+package com.example.rickandmorty.service
+
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitFactory {
+
+    private val BASE_URL = "https://rickandmortyapi.com/api"
+
+
+    //CREATE CONNECTION TO SERVER
+    private val retrofitFactory = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getCharacterService(): CharacterService {
+        return retrofitFactory.create(CharacterService::class.java)
+    }
+
+}
