691c52bf0e460696145b57500095a880a23ada22
Victor Mazzola <victormazzola2019@gmail.com>
2025-03-24 08:00:11 -0300

    feat: add onResponse Toast

--- app/src/main/java/com/example/rickandmorty/screens/CharacterListScreen.kt
+++ app/src/main/java/com/example/rickandmorty/screens/CharacterListScreen.kt
@@ -1,11 +1,11 @@
 package com.example.rickandmorty.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,6 +36,7 @@ import retrofit2.Response
 @Composable
 fun CharacterListScreen(modifier: Modifier = Modifier) {
 
+    var context = LocalContext.current
 
     var characterList by remember {
         mutableStateOf(listOf<Character>())
@@ -47,6 +49,7 @@ fun CharacterListScreen(modifier: Modifier = Modifier) {
     callCharacter.enqueue(object : Callback<Result> {
         override fun onResponse(p0: Call<Result>, result: Response<Result>) {
             characterList = result.body()!!.results!!
+            Toast.makeText(context, "Code: ${result.code()}", Toast.LENGTH_SHORT).show()
         }
 
         override fun onFailure(p0: Call<Result>, p1: Throwable) {
